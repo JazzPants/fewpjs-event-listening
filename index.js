24127b722032fc31767ec64e97c72b7bf44d61f8
JazzPants <leighton.l14@gmail.com>
2021-11-26 16:00:19 +1100

    Done!

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const input = document.getElementById('input'); //node/element that listens
+input.addEventListener('click', function() { //invoke addEventListener() on "input"
+  alert('I was clicked!'); //listen for click, then execute function to do work on the node.
+});
 }
+
+addingEventListener();
